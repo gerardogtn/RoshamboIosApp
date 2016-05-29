4f55b454fac0cac826f47c3f6e5f87bbe1166cfd
Gerardo Teruel <gerardogtn@gmail.com>
2016-05-29 18:51:40 -0500

    :rocket: Add GameSelection enum

---
+++ RoshamboIosApp/GameSelection.swift
@@ -0,0 +1,16 @@
+//
+//  GameSelection.swift
+//  RoshamboIosApp
+//
+//  Created by Gerardo Teruel on 5/29/16.
+//  Copyright © 2016 Gerardo Teruel. All rights reserved.
+//
+
+import Foundation
+
+
+enum GameSelection: Int {
+  case Rock = 0
+  case Paper = 1
+  case Scissors = 2
+}
