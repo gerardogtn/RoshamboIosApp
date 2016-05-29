8f2a31bdeea985d1acf653a13683b4c3aaf823ef
Gerardo Teruel <gerardogtn@gmail.com>
2016-05-29 18:53:43 -0500

    :rocket: Add navigation from MakeSelectionVC to ResultVC

--- RoshamboIosApp/MakeSelectionViewController.swift
+++ RoshamboIosApp/MakeSelectionViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class MakeSelectionViewController: UIViewController {
 
+  var gameResult : GameOutcome!
+  let textOptions = [GameOutcome.Win: "You win!", GameOutcome.Loose: "You lose!", GameOutcome.Tie:  "You tied!"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,61 @@ class MakeSelectionViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  func generateComputerSelection() -> GameSelection {
+    let r = arc4random() % 3
+    return GameSelection(rawValue: Int(r))!
+  }
+  
+  
+  // Function to be executed when a user clicks on rock. It goes to ResultViewController
+  // all programatically
+  @IBAction func onRockClick() {
+    switch generateComputerSelection() {
+    case .Rock:
+      gameResult = .Tie
+      goToTie()
+    case .Paper:
+      gameResult = .Win
+      goToPaperBeatsRock()
+    case .Scissors:
+      gameResult = .Loose
+      goToRockBeatsScissors()
+    }
+  }
 
+  
+  func goToTie() {
+    let controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultViewController") as! ResultViewController
+    
+    controller.textToDisplay = textOptions[gameResult]!
+    controller.imageName = "itsATie"
+    presentViewController(controller, animated: true, completion: nil)
+  }
+  
+  func goToPaperBeatsRock() {
+    let controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultViewController") as! ResultViewController
+    
+    controller.textToDisplay = textOptions[gameResult]!
+    controller.imageName = "PaperCoversRock"
+    presentViewController(controller, animated: true, completion: nil)
+  }
+  
+  func goToRockBeatsScissors() {
+    let controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultViewController") as! ResultViewController
+    
+    controller.textToDisplay = textOptions[gameResult]!
+    controller.imageName = "RockCrushesScissors"
+    presentViewController(controller, animated: true, completion: nil)
+  }
+  
+  func goToScissorsBeatsPaper() {
+    let controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultViewController") as! ResultViewController
+    
+    controller.textToDisplay = textOptions[gameResult]!
+    controller.imageName = "ScissorsCutPaper"
+    presentViewController(controller, animated: true, completion: nil)
+  }
 
 }
 
