bbe6c36f9ba3efbcd755073ff95f7be8482b2315
Gerardo Teruel <gerardogtn@gmail.com>
2016-05-29 00:46:11 -0500

    :rocket: Add ResultViewController

---
+++ RoshamboIosApp/ResultViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.swift
+//  RoshamboIosApp
+//
+//  Created by Gerardo Teruel on 5/29/16.
+//  Copyright © 2016 Gerardo Teruel. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+  
+}
