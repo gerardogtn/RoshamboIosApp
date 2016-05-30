7dd898599c025b9fcce11740b4112cdfecb53369
Gerardo Teruel <gerardogtn@gmail.com>
2016-05-29 22:28:48 -0500

    :rocket: Add method to get result of game between two GameSelections and to generate a random GameSelection

--- RoshamboIosApp/GameSelection.swift
+++ RoshamboIosApp/GameSelection.swift
@@ -9,8 +9,39 @@
 import Foundation
 
 
-enum GameSelection: Int {
-  case Rock = 0
-  case Paper = 1
-  case Scissors = 2
+enum GameSelection: String {
+  case Rock = "rock"
+  case Paper = "paper"
+  case Scissors = "scissors"
+  
+  func result(selection: GameSelection) -> (GameOutcome, String)? {
+    if self == selection {
+      return (.Tie, "itsATie")
+    } else if self == .Rock && selection == .Scissors {
+      return (.Win, "RockCrushesScissors")
+    } else if self == .Paper && selection == .Rock {
+      return (.Win, "PaperCoversRock")
+    } else if self == .Scissors && selection == .Paper {
+      return (.Win, "ScissorsCutPaper")
+    } else if selection == .Rock && self == .Scissors {
+      return (.Loose, "RockCrushesScissors")
+    } else if selection == .Paper && self == .Rock {
+      return (.Loose, "PaperCoversRock")
+    } else if selection == .Scissors && self == .Paper {
+      return (.Loose, "ScissorsCutPaper")
+    }
+    
+    return nil
+  }
+  
+  static func generateRandom() -> GameSelection {
+    let r = arc4random() % 3
+    if (r == 0) {
+      return GameSelection.Rock
+    } else if (r == 1) {
+      return GameSelection.Paper
+    } else {
+      return GameSelection.Scissors
+    }
+  }
 }
